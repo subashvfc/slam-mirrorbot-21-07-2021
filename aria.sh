303f11128b8af48041139eb26e9c78d2b04bdd0a
Mani5GRockers <66015149+Mani5GRockers@users.noreply.github.com>
2021-09-28 13:05:23 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -8,7 +8,7 @@ aria2c --enable-rpc --rpc-listen-all=false --check-certificate=false \
    --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
-   --peer-id-prefix=-qB4360- --user-agent=qBittorrent/4.3.6 --peer-agent=qBittorrent/4.3.6 \
+   --peer-id-prefix=-qB4380- --user-agent=qBittorrent/4.3.8 --peer-agent=qBittorrent/4.3.8 \
    --disk-cache=64M --file-allocation=prealloc --continue=true \
    --max-file-not-found=0 --max-tries=20 --auto-file-renaming=true \
    --bt-enable-lpd=true --seed-time=0.01 --seed-ratio=1.0 \
